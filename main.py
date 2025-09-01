3ee643fe593a8d564c88dc12a4f160d1253da136
Nuthapon <nuthaponm79@gmail.com>
2025-09-01 22:39:29 +0700

    feat: message type

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ if api_key is None:
 # temperature is set to 0 for deterministic responses
 llm = ChatOpenAI(model="gpt-4o-mini", api_key=api_key, temperature=0)
 
-# Create a prompt template
-prompt = ChatPromptTemplate.from_template("เมืืองหลวงของประเทศ {country} ชื่ออะไร")
+# Create a prompt template for translating country names to English
+prompt = ChatPromptTemplate.from_template([
+    ("system", "คุณเป็นผู้เชี่ยวชาญทางด้าน {expertise}"),
+    ("human", "อธิบายเกี่ยวกับ {topic} ให้เข้าใจง่ายๆ ใน {word} ประโยค"),
+])
 
 # Create chain by combining prompt and model
 chain = prompt | llm
 
 # Print the response from the model
 try:
-    response = chain.invoke({"country": "เยอรมัน"})
+    response = chain.invoke({"expertise": "ประวัติศาสตร์", "topic": "สงครามโลกครั้งที่ 2", "word": 3})
     print(response.content)
 except Exception as e:
     print(f"Error occurred: {e}")
